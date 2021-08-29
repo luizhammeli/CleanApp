04a4fa9f29f6b91b85260abbdff88ef6e8d0641a
Luiz Hammerli <luizhammerli@gmail.com>
2021-08-29 18:36:27 -0300

    - Add infra layer

---
+++ InfraTests/InfraTests.swift
@@ -0,0 +1,15 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Luiz Diniz Hammerli on 29/08/21.
+//
+
+import XCTest
+@testable import Infra
+
+class InfraTests: XCTestCase {
+    func test_() {
+        
+    }
+}
