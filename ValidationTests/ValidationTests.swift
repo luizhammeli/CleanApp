449a28e3e435f58307635e8d56f00d6f373812fd
Luiz Hammerli <luizhammerli@gmail.com>
2021-09-19 08:30:40 -0300

    - Add validation target

---
+++ ValidationTests/ValidationTests.swift
@@ -0,0 +1,16 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Luiz Diniz Hammerli on 19/09/21.
+//
+
+import XCTest
+import Presentation
+//@testable import Validation
+
+final class ValidationTests: XCTestCase {
+    func test_() {
+        
+    }
+}
