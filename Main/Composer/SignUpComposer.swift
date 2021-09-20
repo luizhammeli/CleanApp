ea75d2db19b9fb89587e2d658d2cb3be5aa7b88c
Luiz Hammerli <luizhammerli@gmail.com>
2021-09-19 21:48:18 -0300

    - Removes ControllerFactory replacing with composer class

--- Main/Composer/SignUpComposer.swift
+++ Main/Composer/SignUpComposer.swift
@@ -6,12 +6,19 @@
 //
 
 import Foundation
-import Domain
-import Data
 import UI
+import Presentation
+import Validation
+import Data
+import Domain
+import Infra
 
 public final class SignUpComposer {
     static func composeController(with addAccount: AddAccount = UseCaseFactory.makeRemoteAddAccount()) -> SignUpViewController {
-        ControllerFactory.makeSignup(addAccount: addAccount)
+        let emailValidator = EmailValidatorAdapter()
+        guard let controller = SignUpViewController.instantiate() else { fatalError("SignUpViewController should be not nil") }
+        let presenter = SignupPresenter(alertView: WeakVarProxy(controller), loadingView: WeakVarProxy(controller), emailValidator: emailValidator, addAccount: addAccount)
+        controller.signUp = presenter.signup
+        return controller
     }
 }
