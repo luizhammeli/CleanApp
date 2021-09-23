c229ea86cd05fefe3a8ea0db89b33d86503b100d
Luiz Hammerli <luizhammerli@gmail.com>
2021-09-22 21:45:43 -0300

    SignUpComposer refactor

--- Main/Composer/SignUpComposer.swift
+++ Main/Composer/SignUpComposer.swift
@@ -15,9 +15,12 @@ import Infra
 
 public final class SignUpComposer {
     static func composeController(with addAccount: AddAccount = UseCaseFactory.makeRemoteAddAccount()) -> SignUpViewController {
-        let emailValidator = EmailValidatorAdapter()
         guard let controller = SignUpViewController.instantiate() else { fatalError("SignUpViewController should be not nil") }
-        let presenter = SignupPresenter(alertView: WeakVarProxy(controller), loadingView: WeakVarProxy(controller), emailValidator: emailValidator, addAccount: addAccount)
+        let emailValidator = EmailValidatorAdapter()
+        let presenter = SignupPresenter(alertView: WeakVarProxy(controller),
+                                        loadingView: WeakVarProxy(controller),
+                                        emailValidator: emailValidator,
+                                        addAccount: addAccount)
         controller.signUp = presenter.signup
         return controller
     }
