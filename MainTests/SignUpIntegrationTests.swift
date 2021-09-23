4132201dc4ff3a823944e1a1d12773f521a9e336
Luiz Hammerli <luizhammerli@gmail.com>
2021-09-22 22:34:52 -0300

    - Move sut creation to a makeSut method in Main target unit tests

--- MainTests/SignUpIntegrationTests.swift
+++ MainTests/SignUpIntegrationTests.swift
@@ -6,11 +6,24 @@
 //
 
 import XCTest
+import UI
 @testable import Main
 
-class SignUpIntegrationTests: XCTestCase {
+final class SignUpIntegrationTests: XCTestCase {
     func test_ui_presentation_integration_should_not_create_memory_leaks() {        
-        let sut = SignUpComposer.composeController(with: AddAcountSpy())
-        checkMemoryLeak(for: sut)        
+        let (sut, addAcountSpy) = makeSut()
+        sut.loadViewIfNeeded()
+        addAcountSpy.completeWithError(error: .invalidData)        
+    }
+}
+
+extension SignUpIntegrationTests {
+    private func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (SignUpViewController, AddAcountSpy){
+        let addAcountSpy = AddAcountSpy()
+        let sut = SignUpComposer.composeController(with: addAcountSpy)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: addAcountSpy, file: file, line: line)
+
+        return (sut, addAcountSpy)
     }
 }
