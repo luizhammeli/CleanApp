9941452d945f87ab77ce11bc7a06b40179c5ade9
Luiz Hammerli <luizhammerli@gmail.com>
2021-10-02 23:39:57 -0300

    Change AccountModel

--- Domain/Models/AccountModel.swift
+++ Domain/Models/AccountModel.swift
@@ -9,10 +9,8 @@ import Foundation
 
 public struct AccountModel: Model {
     public let accessToken: String
-    public let name: String
     
-    public init(accessToken: String, name: String) {
+    public init(accessToken: String) {
         self.accessToken = accessToken
-        self.name = name
     }
 }
